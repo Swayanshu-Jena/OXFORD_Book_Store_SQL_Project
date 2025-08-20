9fd757950aa226059b08268b228903987219db64
Swayanshu Jena <swayanshu.j00@gmail.com>
2025-08-20 11:46:17 +0530

    Update Bookstore Buisness Problems.sql

--- Bookstore Buisness Problems.sql
+++ Bookstore Buisness Problems.sql
@@ -1,28 +1,24 @@
-Basic Queries
-  
-Retrieve all books in the "Fiction" genre.
-Find books published after the year 1950.
-List all customers from Canada.
-Show orders placed in November 2023.
-Retrieve the total stock of books available.
-Find the details of the most expensive book.
-Show all customers who ordered more than 1 quantity of a book.
-Retrieve all orders where the total amount exceeds $20.
-List all genres available in the Books table.
-Find the book with the lowest stock.
-Calculate the monthly revenue generated from all orders.
-Calculate the total revenue generated from all orders.
+--Basic Queries
+1.  Retrieve all books in the "Fiction" genre.
+2.  Find books published after the year 1950.
+3.  List all customers from Canada.
+4.  Show orders placed in November 2023.
+5.  Retrieve the total stock of books available.
+6.  Find the details of the most expensive book.
+7.  Show all customers who ordered more than 1 quantity of a book.
+8.  Retrieve all orders where the total amount exceeds $20.
+9.  List all genres available in the Books table.
+10. Find the book with the lowest stock.
+11. Calculate the monthly revenue generated from all orders.
+12. Calculate the total revenue generated from all orders.
 
-Advanced Queries
-  
-Retrieve the total number of books sold for each genre.
-Find the average price of books in the "Fantasy" genre.
-List customers who have placed at least 2 orders.
-Find the most frequently ordered book.
-Show the top 3 most expensive books of the 'Fantasy' genre.
-Retrieve the total quantity of books sold by each author.
-List the cities where customers who spent over $30 are located.
-Find the customer who spent the most on orders.
-
-Calculate the stock remaining after fulfilling all orders.
-
+--Advanced Queries
+13.  Retrieve the total number of books sold for each genre.
+14.  Find the average price of books in the "Fantasy" genre.
+15.  List customers who have placed at least 2 orders.
+16  Find the most frequently ordered book.
+17.  Show the top 3 most expensive books of the 'Fantasy' genre.
+18.  Retrieve the total quantity of books sold by each author.
+19.  List the cities where customers who spent over $30 are located.
+20.  Find the customer who spent the most on orders.
+21.  Calculate the stock remaining after fulfilling all orders.
